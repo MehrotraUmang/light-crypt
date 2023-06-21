859cd20d1bc6c64c755e1269c481b2b0824650b0
umang <college.umangmehrotra@gmail.com>
2023-06-21 13:52:38 +0530

    reduced extra funtion call for decrypt method, merged outputs in sigle returnedListGlobal

--- web/logic.js
+++ web/logic.js
@@ -1,16 +1,18 @@
+var returnedListGlobal = []
 function getList() {
-    eel.process(textInp.value)(function (my_list) {
-        document.getElementById('eccprvkey').innerHTML = my_list[0];
-        document.getElementById('eccpubkey').innerHTML = my_list[1];
-        document.getElementById('aeskey').innerHTML = my_list[2];
-        document.getElementById('ciphertext').innerHTML = my_list[3]
+    var pText;
+    pText = textInp.value;
+    eel.process(pText)(function (returnedList) {
+        returnedListGlobal = returnedList;
+        document.getElementById('textInp').innerHTML = returnedListGlobal[4];
+        document.getElementById('eccprvkey').innerHTML = returnedListGlobal[0];
+        document.getElementById('eccpubkey').innerHTML = returnedListGlobal[1];
+        document.getElementById('aeskey').innerHTML = returnedListGlobal[2];
+        document.getElementById('ciphertext').innerHTML = returnedListGlobal[3]
 
     });
 }
 
 function getMessage() {
-    eel.process(textInp.value)(
-        function (my_list1) {
-        document.getElementById('plaintext').innerHTML = my_list1[4];
-    });
+    document.getElementById('plaintext').innerHTML = returnedListGlobal[4];
 }
