948733c48dc6620d3d8dbe5b10cd4c8fc56a6ac4
umang <college.umangmehrotra@gmail.com>
2023-08-05 05:29:47 +0530

    created IAM user, added credentials, permissions to user s3 bucket, successful testing of boto3 on s3 lightcrypt-test-temp

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,10 @@
 import csv
+import os
 from faker import Faker
 
+# Ensure the "generated" directory exists
+if not os.path.exists('generated'):
+    os.makedirs('generated')
 
 # Example usage:
 # generate_csv(num_rows=10, num_columns=5)
@@ -14,12 +18,13 @@ def generate_csv(num_rows, num_columns, filename='random_dataset.csv'):
                for _ in range(num_columns)]
         data.append(row)
 
-    with open(filename, 'w', newline='') as csvfile:
+    filepath = os.path.join('generated', filename)
+    with open(filepath, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
         writer.writerows(data)
 
-    print(f"Generated {num_rows} rows and {num_columns} columns of fake data in {filename}")
+    print(f"Generated {num_rows} rows and {num_columns} columns of fake data in {filepath}")
 
 # Example usage:
 # generate_txt(num_tokens=100, filename="random_text.txt")
@@ -28,9 +33,8 @@ def generate_txt(num_tokens, filename='random_text.txt'):
     words_list = fake.words(num_tokens)
     text = ' '.join(words_list)
     
-    with open(filename, 'w') as file:
+    filepath = os.path.join('generated', filename)
+    with open(filepath, 'w') as file:
         file.write(str(text))
 
-    print(f'Generated {num_tokens} tokens into {filename}')
-
-
+    print(f'Generated {num_tokens} tokens into {filepath}')
